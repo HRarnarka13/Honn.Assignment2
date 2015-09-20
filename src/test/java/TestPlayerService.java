88171bb90385d5883be1bf9c228142636376832b
Eysteinn Gunnlaugsson <eysteinn@Eysteinns-MacBook-Pro.local>
2015-09-20 14:25:47 +0000

    tests from lecture

--- src/test/java/TestPlayerService.java
+++ src/test/java/TestPlayerService.java
@@ -1,6 +1,12 @@
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.logging.Logger;
 
 /**
  * Created by arnarkari on 20/09/15.
@@ -8,8 +14,28 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author arnarkari
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-
+@ContextConfiguration("classpath:app-test-stub.xml")
 public class TestPlayerService extends TestCase {
+    Logger log = Logger.getLogger(TestPlayerService.class.getName());
 
+    @Autowired
+    private PlayerService service;
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testPlayer(){
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        service.addPlayer(player1);
+        service.addPlayer(player2);
+
+        Player playerNew = service.Getplayer(1);
+        assertSame(player1, playerNew);
+    }
 
 }
