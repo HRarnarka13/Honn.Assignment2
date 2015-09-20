d18bba7ae2ad0d77a6b05ea6c799d2c7917fc1a0
Eysteinn Gunnlaugsson <eysteinn@Eysteinns-MacBook-Pro.local>
2015-09-20 14:02:12 +0000

    pull

--- src/test/java/TestPlayerService.java
+++ src/test/java/TestPlayerService.java
@@ -1,11 +1,11 @@
 import junit.framework.TestCase;
 import org.junit.runner.RunWith;
-
 /**
  * Created by arnarkari on 20/09/15.
  *
  * @author arnarkari
  */
+
 @RunWith()
 public class TestPlayerService extends TestCase {
 
