618354addd0ed5e9bc06d1080ff213d3f724ef63
Eysteinn Gunnlaugsson <eysteinn@Eysteinns-MacBook-Pro.local>
2015-09-20 13:54:25 +0000

    dependencies

--- src/test/java/TestPlayerService.java
+++ src/test/java/TestPlayerService.java
@@ -1,8 +1,12 @@
+import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+
 /**
  * Created by arnarkari on 20/09/15.
  *
  * @author arnarkari
  */
+@RunWith()
 public class TestPlayerService extends TestCase {
 
 
