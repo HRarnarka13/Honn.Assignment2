780caa78c85d7bbdc5dba90f18a14c84d0086039
Arnar Kari <arnarkari93@gmail.com>
2015-09-21 17:21:19 +0000

    add reader stuff

---
+++ src/main/java/is/ru/honn/rufan/reader/AbstractReader.java
@@ -0,0 +1,25 @@
+package is.ru.honn.rufan.reader;
+
+/**
+ * Created by arnarkari on 21/09/15.
+ *
+ * @author arnarkari
+ */
+public class AbstractReader implements Reader {
+
+    public Object read() {
+        return null;
+    }
+
+    public Object parse(String content) {
+        return null;
+    }
+
+    public void setURI(String URI) {
+
+    }
+
+    public void setReadHandler(ReadHandler readHandler) {
+
+    }
+}
