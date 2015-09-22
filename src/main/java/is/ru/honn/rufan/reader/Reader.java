0aa9052c340b91b24d69516faaad10fc4fb8f9cc
Arnar Kari <arnarkari93@gmail.com>
2015-09-22 23:17:28 +0000

    javadac for Reader

--- src/main/java/is/ru/honn/rufan/reader/Reader.java
+++ src/main/java/is/ru/honn/rufan/reader/Reader.java
@@ -7,23 +7,25 @@ package is.ru.honn.rufan.reader;
  */
 public interface Reader {
     /**
+     * Read the file (provided URI) and calls parse that parses the content of the file
      *
-     * @return
-     * @throws ReaderException
+     * @return The objects that the parse() function returns
+     * @throws ReaderException if the readerHandler is not set or if there is a exception processing the request for
+     * the file
      */
     public Object read() throws ReaderException;
 
     /**
      * Parse a string to collect the information you need.
-     * Implemented in reader classes that extend AbstractReader.
-     * @param content
-     * @return
+     * Implemented in reader classes that extend this class.
+     * @param content A string to parse
+     * @return An object containing the information wanted.
      */
     public Object parse(String content);
 
     /**
      * Set the URI of the reader
-     * @param URI
+     * @param URI the given URI
      */
     public void setURI(String URI);
 
